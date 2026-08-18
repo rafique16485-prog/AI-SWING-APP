a096939f94e9553b77a1a229adf98e9741e2ed20
Agr india <rafique16485@gmail.com>
2026-08-18 10:31:45 +0530

    feat: route app to top 3 confirmation engine

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Top3Engine from './Top3Engine.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Top3Engine />
   </React.StrictMode>,
 )
