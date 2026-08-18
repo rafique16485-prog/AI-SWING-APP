d5f59e7e9f956b219c46ce06ab91284cd45b703c
Agr india <rafique16485@gmail.com>
2026-08-18 15:08:54 +0530

    Use final Swing Hunter React app entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Top3Engine from './Top3Engine.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Top3Engine />
+    <App />
   </React.StrictMode>,
 )
